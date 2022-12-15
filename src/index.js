4db2bf1c204043d368e4cbaeaae688f21da11db2
UChladecka <ursula.chladecka@decathlon.com>
2022-12-15 15:04:39 +0100

    hmm

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ class Clock extends React.Component {
     render() {
       return (
         <div>
-          <h1>Hello, world!</h1>
+          <h1>Hello, world is this working!</h1>
           <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
         </div>
       );
